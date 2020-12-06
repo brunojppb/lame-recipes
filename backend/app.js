1edb55dc1ae8e48f56a9960f751baab70bf4f584
bruno paulino <brunojppb@gmail.com>
2020-12-06 21:58:13 +0100

    Prepare backend to serve React app under /app/*?

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,21 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// I still want the homepage of our app to be very lean
+// and served from the statics folder. no tricks there.
+app.use(express.static('public'));
+
 // on Production, we serve the build result from our React frontend
+// and any static assset it contains
 app.use(express.static(join(__dirname, '../frontend/build')));
+// Any route after app mounts the React app
+// react-router takes care of the rest
+['/app', '/app/*'].forEach((route) => {
+  app.use(
+    route,
+    express.static(join(__dirname, '../frontend/build/index.html'))
+  );
+});
 
 module.exports = {
   app,
