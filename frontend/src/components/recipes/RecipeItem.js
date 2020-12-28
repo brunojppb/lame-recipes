1c9788cbc89f2eeb3dfa6daec447f91d70ef877e
bruno paulino <brunojppb@gmail.com>
2020-12-28 10:22:49 +0100

    show placeholder for recipes without a cover

--- frontend/src/components/recipes/RecipeItem.js
+++ frontend/src/components/recipes/RecipeItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Routes from "../../routes";
+import recipePlaceholderImg from '../../recipe_placeholder.jpg';
 
 export default function RecipeItem({id, name, cover}) {
 
@@ -11,7 +12,7 @@ export default function RecipeItem({id, name, cover}) {
     <Link to={recipePath} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-2 xl:col-span-1 flex flex-col items-center relative transform hover:scale-105 duration-500">
       <div className="bg-white shadow-lg rounded-md mt-5">
         <img
-          src={cover?.url || 'https://source.unsplash.com/MNtag_eXMKw/1600x900'}
+          src={cover?.url || recipePlaceholderImg}
           className="rounded-t-md object-cover w-full h-40"
           alt="recipe"
         />
