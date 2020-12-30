5b70444078367414687d9c8e80d4fc0b48f47723
bruno paulino <brunojppb@gmail.com>
2020-12-30 12:52:12 +0100

    add open graph tags and cover

--- frontend/src/components/user/SignupPage.js
+++ frontend/src/components/user/SignupPage.js
@@ -85,7 +85,7 @@ export default function SignupPage() {
                      type="password"
                      required
                      className="appearance-none rounded-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-2"
-                     placeholder="Password"/>
+                     placeholder="Password confirmation"/>
             </div>
           </div>
 
