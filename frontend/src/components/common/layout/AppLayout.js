d90ed46df86cd7ec425ee06b0af6162e36b4e0eb
bruno paulino <brunojppb@gmail.com>
2021-01-10 17:18:21 +0100

    remove custom variable for screen height

--- frontend/src/components/common/layout/AppLayout.js
+++ frontend/src/components/common/layout/AppLayout.js
@@ -6,8 +6,8 @@ export default function AppLayout({children}) {
   return (
     <>
       <Navbar/>
-      <div className="flex w-full full-screen-height pb-20 sm:pb-0 sm:pt-16">
-        <section className="w-full p-4 overflow-y-auto">
+      <div className="flex w-full h-screen pb-20 sm:pb-0 sm:pt-16">
+        <section className="w-full p-4">
           {children}
         </section>
       </div>
